05f7ba5da5b5dd2cc3045289e76f418fb8848dfc
rollenholt <renwenchao888@gmail.com>
2015-10-08 10:03:25 +0800

    add acl and operation log scheme

--- scheme/scheme.sql
+++ scheme/scheme.sql
@@ -18,5 +18,19 @@ create table application(
 
 create table application_acl(
   id int(10) unsigned not null AUTO_INCREMENT,
-
+  application_code varchar(64) not null comment '',
+  state tinyint(4) not null comment '',
+  url varchar(255) not null comment '',
+  authorized_application varchar(255) not null comment '',
+  authorized_ip varchar(255) not null comment '',
+  PRIMARY KEY (id),
+  key idx_application_code(application_code)
 )engine=innodb default charset=utf8mb4 comment 'application acl table';
+
+create table operation_log(
+  id int(10) UNSIGNED not null AUTO_INCREMENT,
+  operator varchar(32) not null comment '操作人',
+  operate_time datetime not null comment '操作时间',
+  detail text not null comment '操作详情',
+  PRIMARY KEY (id)
+)engine=innodb default charset=utf8 comment 'operation log table';
